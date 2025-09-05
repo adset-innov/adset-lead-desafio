0bc1b37a20af753a5b16bba1cb8cab33fabcc7a8
Lucas Tinoco <lucas.tinoco@hotmail.com>
2025-09-04 22:06:10 -0300

    refactor(backend): convert constructor to primary constructor on PagedResult

--- src/api/AdSet.Lead/AdSet.Lead.Domain/VOs/PagedResult.cs
+++ src/api/AdSet.Lead/AdSet.Lead.Domain/VOs/PagedResult.cs
@@ -1,19 +1,10 @@
 ﻿namespace AdSet.Lead.Domain.VOs;
 
-public sealed class PagedResult<T>
+public sealed class PagedResult<T>(IEnumerable<T> items, int totalCount, int pageNumber, int pageSize)
 {
-    public IEnumerable<T> Items { get; }
-    public int TotalCount { get; }
-    public int PageNumber { get; }
-    public int PageSize { get; }
-
-    public PagedResult(IEnumerable<T> items, int totalCount, int pageNumber, int pageSize)
-    {
-        Items = items;
-        TotalCount = totalCount;
-        PageNumber = pageNumber;
-        PageSize = pageSize;
-    }
-
+    public IEnumerable<T> Items { get; } = items;
+    public int TotalCount { get; } = totalCount;
+    public int PageNumber { get; } = pageNumber;
+    public int PageSize { get; } = pageSize;
     public int TotalPages => (int)Math.Ceiling((double)TotalCount / PageSize);
 }
